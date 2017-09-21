0992f6e7ed056710d05026be3c9dc40cf84d36c7
Vinci-da-Gama <11582145@student.uts.edu.au>
2017-09-21 16:27:53 +1000

    home page

---
+++ MvcModulePj/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcModulePj
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jslibs").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/semantic.js"
+            ));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/csslibs").Include(
+                      "~/Content/semantic.css"
+            ));
+
+            bundles.Add(new StyleBundle("~/Content/custCss").Include(
+                      "~/Content/Site.css"
+            ));
+        }
+    }
+}
---
+++ MvcModulePj/Controllers/HomeIndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcModulePj.Controllers
+{
+    public class HomeIndexController : Controller
+    {
+        // GET: HomeIndex
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
