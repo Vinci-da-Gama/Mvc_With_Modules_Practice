d1bbf4ae58682f86f7146c4ccb9363bc5f9087e4
Vinci-da-Gama <11582145@student.uts.edu.au>
2017-09-21 15:35:09 +1000

    add Modules_area

---
+++ MvcModulePj/Areas/Modules_Areas/Modules_AreasAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MvcModulePj.Areas.Modules_Areas
+{
+    public class Modules_AreasAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Modules_Areas";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Modules_Areas_default",
+                "Modules_Areas/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
